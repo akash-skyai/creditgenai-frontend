2b46e74d4504a576b01022e8e8149e5f29a9441f
Akash-skyai <Akash.w@skyaigiants.com>
2026-07-31 18:14:15 +0530

    chore: configure testing environment and root providers

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { PrimeReactProvider } from '@primereact/core'
 import './styles/globals.scss'
 import App from './App.tsx'
 
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <PrimeReactProvider>
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
+    </PrimeReactProvider>
   </StrictMode>,
 )
